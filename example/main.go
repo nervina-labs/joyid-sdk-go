db2dc8e33114db2c93df402bb9fe220a69e2fbbf
duanyytop <duanyytop@gmail.com>
2023-01-09 20:18:43 +0800

    feat: Add extension secp256k1 subkey example

--- example/main.go
+++ example/main.go
@@ -13,15 +13,15 @@ import (
 
 	"github.com/nervina-labs/joyid-sdk-go/aggregator"
 	"github.com/nervina-labs/joyid-sdk-go/crypto/alg"
+	"github.com/nervina-labs/joyid-sdk-go/crypto/secp256k1"
 	"github.com/nervina-labs/joyid-sdk-go/crypto/secp256r1"
 	"github.com/nervina-labs/joyid-sdk-go/signer"
 	"github.com/nervina-labs/joyid-sdk-go/utils"
 )
 
 func main() {
-	err := AddSecp256r1Subkey()
-	if err != nil {
-		fmt.Printf("transfer error: %v", err)
+	if err := SubkeyTransferWithK1(); err != nil {
+		fmt.Printf("main error: %v", err)
 	}
 }
 
@@ -74,7 +74,7 @@ func NativeTransferWithR1() error {
 	if err != nil {
 		return err
 	}
-	fmt.Println("the tx hash of native transfer with secp256r1: " + hexutil.Encode(hash.Bytes()))
+	fmt.Println("the tx hash of secp256r1 native unlock transfer: " + hexutil.Encode(hash.Bytes()))
 	return nil
 }
 
@@ -122,7 +122,7 @@ func NativeTransferWithK1() error {
 	if err != nil {
 		return err
 	}
-	fmt.Println("the tx hash of native transfer with secp256k1: " + hexutil.Encode(hash.Bytes()))
+	fmt.Println("the tx hash of secp256k1 native unlock transfer: " + hexutil.Encode(hash.Bytes()))
 	return nil
 }
 
@@ -191,11 +191,74 @@ func SubkeyTransferWithR1() error {
 	if err != nil {
 		return err
 	}
-	fmt.Println("the tx hash of subkey transfer with secp256r1: " + hexutil.Encode(hash.Bytes()))
+	fmt.Println("the tx hash of secp256r1 subkey unlock transfer: " + hexutil.Encode(hash.Bytes()))
 	return nil
 }
 
-func AddSecp256r1Subkey() error {
+func SubkeyTransferWithK1() error {
+	// senderPrivKey := "4271c23380932c74a041b4f56779e5ef60e808a127825875f906260f1f657761"
+	sender := "ckt1qqr4jkln4qmtmdle82g6vm9jer967rvq069danwunkgs4tr0pfws7qgqqfjsplqwsm75nmmal39jth7k2n4v4t2nlvmef595"
+	senderSubkeyPrivKey := "86f850ed0e871df5abb188355cd6fe00809063c6bdfd822f420f2d0a8a7c985d"
+	receiver := "ckt1qqr4jkln4qmtmdle82g6vm9jer967rvq069danwunkgs4tr0pfws7qgqqfjsplqwsm75nmmal39jth7k2n4v4t2nlvmef595"
+	network := types.NetworkTest
+	client, err := rpc.Dial("https://testnet.ckb.dev/rpc")
+	if err != nil {
+		return err
+	}
+	iterator, err := collector.NewLiveCellIteratorFromAddress(client, sender)
+	if err != nil {
+		return err
+	}
+
+	// build transaction
+	builder := builder.NewCkbTransactionBuilder(network, iterator)
+	builder.FeeRate = 5000
+	if err := builder.AddOutputByAddress(receiver, 15200000000); err != nil {
+		return err
+	}
+	builder.AddChangeOutputByAddress(sender)
+	senderAddr, err := address.Decode(sender)
+	if err != nil {
+		return err
+	}
+	cotaCellDep, err := utils.CotaCellDep("https://testnet.ckb.dev/indexer", senderAddr)
+	if err != nil {
+		return err
+	}
+	builder.AddCellDep(cotaCellDep)
+	builder.AddCellDep(utils.JoyIDLockCellDep(network))
+	txWithGroups, err := builder.Build()
+	if err != nil {
+		return err
+	}
+
+	// Init WitnessArgs
+	tx := txWithGroups.TxView
+	witnessArgs := types.WitnessArgs{
+		Lock: []byte{},
+	}
+	tx.Witnesses = [][]byte{witnessArgs.Serialize()}
+
+	// Add subkey unlock smt to WitnessArgs.Output
+	algKey := signer.AlgPrivKey{
+		PrivKey: senderSubkeyPrivKey,
+		Alg:     alg.Secp256k1,
+	}
+	signer.BuildOutputTypeWithSubkeySmt(tx, algKey, senderAddr)
+
+	// Sign transaction
+	signer.SignSubkeyUnlockTx(tx, algKey, nil)
+
+	// send transaction
+	hash, err := client.SendTransaction(context.Background(), tx)
+	if err != nil {
+		return err
+	}
+	fmt.Println("the tx hash of secp256k1 subkey unlock transfer: " + hexutil.Encode(hash.Bytes()))
+	return nil
+}
+
+func AddSecp256r1SubkeyWithNativeUnlock() error {
 	senderPrivKey := "4271c23380932c74a041b4f56779e5ef60e808a127825875f906260f1f657761"
 	sender := "ckt1qqr4jkln4qmtmdle82g6vm9jer967rvq069danwunkgs4tr0pfws7qgqq9sfrkfah2cj79nyp7e6p283ualq8779rsgww3jf"
 	senderSubkeyPrivKey := "86f850ed0e871df5abb188355cd6fe00809063c6bdfd822f420f2d0a8a7c985d"
@@ -275,33 +338,24 @@ func AddSecp256r1Subkey() error {
 	if err != nil {
 		return err
 	}
-	fmt.Println("the tx hash of adding extension secp256r1 subkey with secp256r1 unlock: " + hexutil.Encode(hash.Bytes()))
+	fmt.Println("the tx hash of adding extension secp256r1 subkey with secp256r1 native unlock: " + hexutil.Encode(hash.Bytes()))
 	return nil
 }
 
-func SubkeyTransferWithK1() error {
-	// senderPrivKey := "4271c23380932c74a041b4f56779e5ef60e808a127825875f906260f1f657761"
+func AddSecp256k1SubkeyWithNativeUnlock() error {
+	senderPrivKey := "4271c23380932c74a041b4f56779e5ef60e808a127825875f906260f1f657761"
 	sender := "ckt1qqr4jkln4qmtmdle82g6vm9jer967rvq069danwunkgs4tr0pfws7qgqqfjsplqwsm75nmmal39jth7k2n4v4t2nlvmef595"
-	senderSubkeyPrivKey := "7b9d3f2f356ead86d5f04fc90e8096d706247027c349ac75357094459d8724b9"
-	receiver := "ckt1qqr4jkln4qmtmdle82g6vm9jer967rvq069danwunkgs4tr0pfws7qgqqfjsplqwsm75nmmal39jth7k2n4v4t2nlvmef595"
+	senderSubkeyPrivKey := "86f850ed0e871df5abb188355cd6fe00809063c6bdfd822f420f2d0a8a7c985d"
 	network := types.NetworkTest
 	client, err := rpc.Dial("https://testnet.ckb.dev/rpc")
 	if err != nil {
 		return err
 	}
-	iterator, err := collector.NewLiveCellIteratorFromAddress(client, sender)
+	senderAddr, err := address.Decode(sender)
 	if err != nil {
 		return err
 	}
-
-	// build transaction
-	builder := builder.NewCkbTransactionBuilder(network, iterator)
-	builder.FeeRate = 5000
-	if err := builder.AddOutputByAddress(receiver, 15200000000); err != nil {
-		return err
-	}
-	builder.AddChangeOutputByAddress(sender)
-	senderAddr, err := address.Decode(sender)
+	cotaCell, err := utils.GetCotaLiveCell("https://testnet.ckb.dev/indexer", senderAddr)
 	if err != nil {
 		return err
 	}
@@ -309,36 +363,62 @@ func SubkeyTransferWithK1() error {
 	if err != nil {
 		return err
 	}
-	builder.AddCellDep(cotaCellDep)
-	builder.AddCellDep(utils.JoyIDLockCellDep(network))
-	txWithGroups, err := builder.Build()
+	input := &types.CellInput{
+		PreviousOutput: cotaCell.OutPoint,
+		Since:          0x0,
+	}
+	fee := uint64(2000)
+	output := &types.CellOutput{
+		Capacity: cotaCell.Output.Capacity - fee,
+		Lock:     cotaCell.Output.Lock,
+		Type:     cotaCell.Output.Type,
+	}
+
+	rpc := aggregator.NewRPCClient("http://127.0.0.1:3030")
+	pubkeyHash := secp256k1.ImportKey(senderSubkeyPrivKey).PubkeyHash()
+	extensionSubkeySmt, err := rpc.GetExtensionSubkeySmt(senderAddr, pubkeyHash, alg.Secp256k1, 1)
 	if err != nil {
 		return err
 	}
-
-	// Init WitnessArgs
-	tx := txWithGroups.TxView
-	witnessArgs := types.WitnessArgs{
-		Lock: []byte{},
+	extSubkeySmtEntry, err := utils.HexToBytes(extensionSubkeySmt.ExtensionSmtEntry)
+	if err != nil {
+		return err
 	}
-	tx.Witnesses = [][]byte{witnessArgs.Serialize()}
+	witnessInputType := []byte{0xF0}
+	witnessInputType = append(witnessInputType, extSubkeySmtEntry...)
+	cotaSmtRoot, err := utils.HexToBytes(extensionSubkeySmt.SmtRootHash)
+	if err != nil {
+		return err
+	}
+	cotaOuputData := []byte{0x02}
+	cotaOuputData = append(cotaOuputData, cotaSmtRoot...)
+	witnessArgs := types.WitnessArgs{
+		Lock:      []byte{},
+		InputType: witnessInputType,
+	}
+	tx := &types.Transaction{
+		Version:     0x0,
+		Inputs:      []*types.CellInput{input},
+		Outputs:     []*types.CellOutput{output},
+		OutputsData: [][]byte{cotaOuputData},
+		CellDeps:    []*types.CellDep{cotaCellDep, utils.JoyIDLockCellDep(network), utils.CotaTypeCellDep(network)},
+		Witnesses:   [][]byte{witnessArgs.Serialize()},
+	}
 
-	// Add subkey unlock smt to WitnessArgs.Output
+	// Build webAuthn message
 	algKey := signer.AlgPrivKey{
-		PrivKey: senderSubkeyPrivKey,
+		PrivKey: senderPrivKey,
 		Alg:     alg.Secp256k1,
 	}
-	signer.BuildOutputTypeWithSubkeySmt(tx, algKey, senderAddr)
-
 	// Sign transaction
-	signer.SignSubkeyUnlockTx(tx, algKey, nil)
+	signer.SignNativeUnlockTx(tx, algKey, nil)
 
 	// send transaction
 	hash, err := client.SendTransaction(context.Background(), tx)
 	if err != nil {
 		return err
 	}
-	fmt.Println("the tx hash of subkey transfer with secp256k1: " + hexutil.Encode(hash.Bytes()))
+	fmt.Println("the tx hash of adding extension secp256k1 subkey with secp256k1 native unlock: " + hexutil.Encode(hash.Bytes()))
 	return nil
 }
 
