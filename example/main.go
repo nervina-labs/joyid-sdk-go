a24a37a0bba0dee9089c0f651f759fc48cdd6fb8
duanyytop <duanyytop@gmail.com>
2023-01-09 11:20:10 +0800

    feat: Add subkey unlock example

--- example/main.go
+++ example/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/nervosnetwork/ckb-sdk-go/v2/address"
 	"github.com/nervosnetwork/ckb-sdk-go/v2/collector"
 	"github.com/nervosnetwork/ckb-sdk-go/v2/collector/builder"
 	"github.com/nervosnetwork/ckb-sdk-go/v2/rpc"
@@ -16,7 +17,7 @@ import (
 )
 
 func main() {
-	err := NativeTransferWithK1()
+	err := SubkeyTransferWithK1()
 	if err != nil {
 		fmt.Printf("transfer error: %v", err)
 	}
@@ -27,7 +28,7 @@ func NativeTransferWithR1() error {
 	sender := "ckt1qrfrwcdnvssswdwpn3s9v8fp87emat306ctjwsm3nmlkjg8qyza2cqgqq9sfrkfah2cj79nyp7e6p283ualq8779rscnjmrj"
 	receiver := "ckt1qrfrwcdnvssswdwpn3s9v8fp87emat306ctjwsm3nmlkjg8qyza2cqgqq9sfrkfah2cj79nyp7e6p283ualq8779rscnjmrj"
 	network := types.NetworkTest
-	client, err := rpc.Dial("https://testnet.ckb.dev")
+	client, err := rpc.Dial("https://testnet.ckb.dev/rpc")
 	if err != nil {
 		return err
 	}
@@ -66,14 +67,12 @@ func NativeTransferWithR1() error {
 	}
 	signer.SignNativeUnlockTx(tx, algKey, webAuthnMsg)
 
-	fmt.Println(hexutil.Encode(tx.Witnesses[0]))
-
 	// send transaction
 	hash, err := client.SendTransaction(context.Background(), tx)
 	if err != nil {
 		return err
 	}
-	fmt.Println("the transaction hash of native transfer with secp256r1: " + hexutil.Encode(hash.Bytes()))
+	fmt.Println("the tx hash of native transfer with secp256r1: " + hexutil.Encode(hash.Bytes()))
 	return nil
 }
 
@@ -82,7 +81,7 @@ func NativeTransferWithK1() error {
 	sender := "ckt1qrfrwcdnvssswdwpn3s9v8fp87emat306ctjwsm3nmlkjg8qyza2cqgqqfjsplqwsm75nmmal39jth7k2n4v4t2nlvty4750"
 	receiver := "ckt1qrfrwcdnvssswdwpn3s9v8fp87emat306ctjwsm3nmlkjg8qyza2cqgqqfjsplqwsm75nmmal39jth7k2n4v4t2nlvty4750"
 	network := types.NetworkTest
-	client, err := rpc.Dial("https://testnet.ckb.dev")
+	client, err := rpc.Dial("https://testnet.ckb.dev/rpc")
 	if err != nil {
 		return err
 	}
@@ -121,7 +120,139 @@ func NativeTransferWithK1() error {
 	if err != nil {
 		return err
 	}
-	fmt.Println("the transaction hash of native transfer with secp256k1: " + hexutil.Encode(hash.Bytes()))
+	fmt.Println("the tx hash of native transfer with secp256k1: " + hexutil.Encode(hash.Bytes()))
+	return nil
+}
+
+func SubkeyTransferWithR1() error {
+	// senderPrivKey := "4271c23380932c74a041b4f56779e5ef60e808a127825875f906260f1f657761"
+	sender := "ckt1qrfrwcdnvssswdwpn3s9v8fp87emat306ctjwsm3nmlkjg8qyza2cqgqq9sfrkfah2cj79nyp7e6p283ualq8779rscnjmrj"
+	senderSubkeyPrivKey := "86f850ed0e871df5abb188355cd6fe00809063c6bdfd822f420f2d0a8a7c985d"
+	receiver := "ckt1qrfrwcdnvssswdwpn3s9v8fp87emat306ctjwsm3nmlkjg8qyza2cqgqq9sfrkfah2cj79nyp7e6p283ualq8779rscnjmrj"
+	network := types.NetworkTest
+	client, err := rpc.Dial("https://testnet.ckb.dev/rpc")
+	if err != nil {
+		return err
+	}
+	iterator, err := collector.NewLiveCellIteratorFromAddress(client, sender)
+	if err != nil {
+		return err
+	}
+
+	// build transaction
+	builder := builder.NewCkbTransactionBuilder(network, iterator)
+	builder.FeeRate = 5000
+	if err := builder.AddOutputByAddress(receiver, 15200000000); err != nil {
+		return err
+	}
+	builder.AddChangeOutputByAddress(sender)
+	senderAddr, err := address.Decode(sender)
+	if err != nil {
+		return err
+	}
+	cotaCellDep, err := utils.CotaCellDep("https://testnet.ckb.dev/indexer", senderAddr)
+	if err != nil {
+		return err
+	}
+	builder.AddCellDep(cotaCellDep)
+	builder.AddCellDep(utils.JoyIDCellDep(network))
+	txWithGroups, err := builder.Build()
+	if err != nil {
+		return err
+	}
+
+	// Init WitnessArgs
+	tx := txWithGroups.TxView
+	witnessArgs := types.WitnessArgs{
+		Lock: []byte{},
+	}
+	tx.Witnesses = [][]byte{witnessArgs.Serialize()}
+
+	// Add subkey unlock smt to WitnessArgs.Output
+	algKey := signer.AlgPrivKey{
+		PrivKey: senderSubkeyPrivKey,
+		Alg:     alg.Secp256r1,
+	}
+	signer.BuildOutputTypeWithSubkeySmt(tx, algKey, senderAddr)
+
+	// Build webAuthn message
+	webAuthnMsg, err := generateWebAuthnMsg(tx)
+	if err != nil {
+		return err
+	}
+
+	// Sign transaction
+	signer.SignSubkeyUnlockTx(tx, algKey, webAuthnMsg)
+
+	// send transaction
+	hash, err := client.SendTransaction(context.Background(), tx)
+	if err != nil {
+		return err
+	}
+	fmt.Println("the tx hash of subkey transfer with secp256r1: " + hexutil.Encode(hash.Bytes()))
+	return nil
+}
+
+func SubkeyTransferWithK1() error {
+	// senderPrivKey := "4271c23380932c74a041b4f56779e5ef60e808a127825875f906260f1f657761"
+	sender := "ckt1qrfrwcdnvssswdwpn3s9v8fp87emat306ctjwsm3nmlkjg8qyza2cqgqqfjsplqwsm75nmmal39jth7k2n4v4t2nlvty4750"
+	senderSubkeyPrivKey := "7b9d3f2f356ead86d5f04fc90e8096d706247027c349ac75357094459d8724b9"
+	receiver := "ckt1qrfrwcdnvssswdwpn3s9v8fp87emat306ctjwsm3nmlkjg8qyza2cqgqqfjsplqwsm75nmmal39jth7k2n4v4t2nlvty4750"
+	network := types.NetworkTest
+	client, err := rpc.Dial("https://testnet.ckb.dev/rpc")
+	if err != nil {
+		return err
+	}
+	iterator, err := collector.NewLiveCellIteratorFromAddress(client, sender)
+	if err != nil {
+		return err
+	}
+
+	// build transaction
+	builder := builder.NewCkbTransactionBuilder(network, iterator)
+	builder.FeeRate = 5000
+	if err := builder.AddOutputByAddress(receiver, 15200000000); err != nil {
+		return err
+	}
+	builder.AddChangeOutputByAddress(sender)
+	senderAddr, err := address.Decode(sender)
+	if err != nil {
+		return err
+	}
+	cotaCellDep, err := utils.CotaCellDep("https://testnet.ckb.dev/indexer", senderAddr)
+	if err != nil {
+		return err
+	}
+	builder.AddCellDep(cotaCellDep)
+	builder.AddCellDep(utils.JoyIDCellDep(network))
+	txWithGroups, err := builder.Build()
+	if err != nil {
+		return err
+	}
+
+	// Init WitnessArgs
+	tx := txWithGroups.TxView
+	witnessArgs := types.WitnessArgs{
+		Lock: []byte{},
+	}
+	tx.Witnesses = [][]byte{witnessArgs.Serialize()}
+
+	// Add subkey unlock smt to WitnessArgs.Output
+	algKey := signer.AlgPrivKey{
+		PrivKey: senderSubkeyPrivKey,
+		Alg:     alg.Secp256k1,
+	}
+	signer.BuildOutputTypeWithSubkeySmt(tx, algKey, senderAddr)
+
+	// Sign transaction
+	signer.SignSubkeyUnlockTx(tx, algKey, nil)
+
+	// send transaction
+	hash, err := client.SendTransaction(context.Background(), tx)
+	if err != nil {
+		return err
+	}
+	fmt.Println("the tx hash of subkey transfer with secp256k1: " + hexutil.Encode(hash.Bytes()))
 	return nil
 }
 
